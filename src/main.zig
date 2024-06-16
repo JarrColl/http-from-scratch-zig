1e0d807ab88ea6d758a72e0334f2dad45298d64d
JarrColl <jarronc@protonmail.com>
2024-06-16 21:55:20 +1000

    extract url path

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,4 @@
 const std = @import("std");
-// Uncomment this block to pass the first stage
 const net = std.net;
 
 pub fn main() !void {
@@ -8,15 +7,33 @@ pub fn main() !void {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     try stdout.print("Logs from your program will appear here!\n", .{});
 
-    // Uncomment this block to pass the first stage
     const address = try net.Address.resolveIp("127.0.0.1", 4221);
+
     var listener = try address.listen(.{
         .reuse_address = true,
     });
     defer listener.deinit();
 
-    var connection = try listener.accept();
-    try connection.stream.writeAll("HTTP/1.1 200 OK\r\n\r\n");
+    const connection = try listener.accept();
+    defer connection.stream.close();
+
+    var request_buffer: [1024]u8 = undefined;
+
+    _ = try connection.stream.read(&request_buffer);
+
+    var it = std.mem.split(u8, &request_buffer, " ");
+    var i: u8 = 0; // Is it possible to send the request that overflows this and crashes.
+    while (it.next()) |split| : (i += 1) {
+        if (i == 1) {
+            if (std.mem.eql(u8, split, "/")) {
+                try connection.stream.writeAll("HTTP/1.1 200 OK\r\n\r\n");
+            } else {
+                try connection.stream.writeAll("HTTP/1.1 404 Not Found\r\n\r\n");
+            }
+
+            std.debug.print("{s}", .{split});
+        }
+    }
 
     try stdout.print("client connected!", .{});
 }
