10c3044c195a687ee461266be9dec45af35ec473
JarrColl <jarronc@protonmail.com>
2024-06-24 22:27:27 +1000

    encoding headers

--- src/main.zig
+++ src/main.zig
@@ -4,11 +4,28 @@ const std = @import("std");
 // const clap = @import("clap");
 const net = std.net;
 const thread = std.Thread;
+const assert = std.debug.assert;
 
 const ServerError = error{ArgsError};
 
-const ResponseCode = enum([]u8)  {
-    @"200" = "http 200  ok",
+const ResponseCode = enum {
+    @"200",
+    @"201",
+    @"400",
+    @"404",
+    @"500",
+
+    pub const ResponseTextTable = [@typeInfo(ResponseCode).Enum.fields.len][:0]const u8{
+        "HTTP/1.1 200 OK",
+        "HTTP/1.1 201 Created",
+        "HTTP/1.1 400 Bad Request",
+        "HTTP/1.1 404 Not Found",
+        "HTTP/1.1 500 Internal Server Error",
+    };
+
+    pub fn str(self: ResponseCode) []const u8 {
+        return ResponseTextTable[@intFromEnum(self)];
+    }
 };
 
 const Args = struct { directory: ?[]const u8 };
@@ -17,26 +34,73 @@ const Header = struct {
     name: []const u8,
     value: []const u8,
 };
-const SUPPORTED_ENCODINGS = [_][]const u8{"gzip"};
+
+const Encoding = enum {
+    GZip,
+
+    pub const EncodingTypeTable = [@typeInfo(Encoding).Enum.fields.len][:0]const u8{
+        "gzip",
+    };
+
+    pub fn str(self: Encoding) []const u8 {
+        return EncodingTypeTable[@intFromEnum(self)];
+    }
+
+    pub fn fromString(name: []const u8) ?Encoding {
+        inline for (EncodingTypeTable, 0..) |valid_name, i| {
+            if (std.mem.eql(u8, name, valid_name)) {
+                return @enumFromInt(i);
+            }
+        }
+        return null;
+    }
+};
+
+const Response = struct {
+    code: ?ResponseCode = null,
+    content_type: ?[]const u8 = null,
+    content: ?[]const u8 = null,
+    // headers: ?[]Header = null,
+    encoding: ?Encoding = null,
+
+    pub fn sendResponse(self: Response, connection: net.Server.Connection) !void {
+        std.debug.print("encoding: {?}\n", .{self.encoding});
+        assert(self.code != null);
+
+        try connection.stream.writeAll(self.code.?.str());
+        try connection.stream.writeAll("\r\n");
+
+        if (self.content_type) |content_type| {
+            assert(self.content != null);
+
+            try connection.stream.writer().print("Content-Type: {s}\r\n", .{content_type});
+            if (self.encoding) |encoding| {
+                try connection.stream.writer().print("Content-Encoding: {s}\r\n", .{encoding.str()});
+
+                try connection.stream.writeAll("\r\n");
+                try connection.stream.writeAll(self.content.?);
+            } else {
+                try connection.stream.writeAll("\r\n");
+                try connection.stream.writeAll(self.content.?);
+            }
+        } else {
+            try connection.stream.writeAll("\r\n");
+        }
+    }
+};
+// try connection.stream.writer().print("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {d}\r\n\r\n{s}", .{ header.value.len, header.value });
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    // try stdout.print("Logs from your program will appear here!\n", .{});
 
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     const alloc = gpa.allocator();
     defer _ = gpa.deinit();
 
-    // const args = parseArgs(alloc) catch |err| {
-    //     if (err == ServerError.ArgsError) return;
-    //     return err;
-    // };
-    // std.debug.print("file: {s}\n", .{args.directory});
     const args = parseArgs() catch |err| {
         if (err == ServerError.ArgsError) return;
         return err;
     };
-    // std.debug.print("file: {s}\n", .{args.directory});
 
     var thread_pool: thread.Pool = undefined;
     try thread_pool.init(.{ .allocator = alloc });
@@ -54,8 +118,6 @@ pub fn main() !void {
 
         try thread_pool.spawn(processConnection, .{ args, connection, alloc });
         std.debug.print("Queue Size: {}\n", .{thread_pool.run_queue.len()});
-        // const handle = try thread.spawn(.{}, processConnection, .{connection});
-        // handle.detach();
     }
 }
 
@@ -82,45 +144,8 @@ pub fn parseArgs() !Args {
         }
     }
 
-    // try stderr.writeAll(help_message);
-    // return ServerError.ArgsError;
     return Args{ .directory = null };
 }
-
-// pub fn parseArgs(alloc: std.mem.Allocator) !Args {
-//
-//     const params = comptime clap.parseParamsComptime(
-//         \\-h, --help             Display this help and exit.
-//         \\-d, --directory <str>       An option parameter which can be specified multiple times.
-//     );
-//
-//     var diag = clap.Diagnostic{};
-//     var res = clap.parse(clap.Help, &params, clap.parsers.default, .{
-//         .diagnostic = &diag,
-//         .allocator = alloc,
-//     }) catch |err| {
-//         // Report useful error and exit
-//         diag.report(std.io.getStdErr().writer(), err) catch {};
-//         return err;
-//     };
-//
-//     defer res.deinit();
-//
-//     if (res.args.help != 0) {
-//         try clap.help(std.io.getStdErr().writer(), clap.Help, &params, .{});
-//         return ServerError.ArgsError;
-//     }
-//     if (res.args.directory) |f| {
-//         std.fs.cwd().access(f, .{}) catch {
-//             try std.io.getStdErr().writeAll("The directory could not be accessed. Please check if it exists.");
-//             return ServerError.ArgsError;
-//         };
-//         return .{ .directory = f };
-//     } else {
-//         try std.io.getStdErr().writeAll("-d, --directory argument is required.");
-//         return ServerError.ArgsError;
-//     }
-// }
 
 pub fn handleError(connection: net.Server.Connection) void {
     connection.stream.writeAll("HTTP/1.1 500 Internal Server Error\r\n\r\n") catch return;
@@ -128,6 +153,7 @@ pub fn handleError(connection: net.Server.Connection) void {
 
 pub fn processConnection(args: Args, connection: net.Server.Connection, alloc: std.mem.Allocator) void {
     defer connection.stream.close();
+    var response = Response{};
 
     var request_buffer: [1024]u8 = undefined;
     _ = connection.stream.read(&request_buffer) catch {
@@ -149,41 +175,55 @@ pub fn processConnection(args: Args, connection: net.Server.Connection, alloc: s
         return;
     };
 
+    // Set the encoding in the response struct if it was sent.
+    for (headers) |header| {
+        if (std.ascii.eqlIgnoreCase(header.name, "Accept-Encoding")) {
+            response.encoding = Encoding.fromString(header.value);
+        }
+    }
+
     const body = entire_request_iterator.next() orelse "";
 
     if (std.mem.eql(u8, method, "GET")) {
-        get(route, headers, args, connection, alloc) catch |err| {
+        get(route, headers, args, &response, connection, alloc) catch |err| {
             std.debug.print("Get error: {}", .{err});
             handleError(connection);
             return;
         };
     } else if (std.mem.eql(u8, method, "POST")) {
-        post(route, headers, body, args, connection, alloc) catch |err| {
+        post(route, headers, body, args, &response, connection, alloc) catch |err| {
             std.debug.print("Post error: {}", .{err});
             handleError(connection);
             return;
         };
     } else {
-        connection.stream.writeAll("HTTP/1.1 400 Bad Request\r\n\r\n") catch handleError(connection);
-        return;
+        // connection.stream.writeAll("HTTP/1.1 400 Bad Request\r\n\r\n") catch handleError(connection);
+        response.code = ResponseCode.@"400";
     }
+
+    response.sendResponse(connection) catch handleError(connection);
 }
 
-pub fn get(route: []const u8, headers: []const Header, args: Args, connection: net.Server.Connection, alloc: std.mem.Allocator) !void {
+pub fn get(route: []const u8, headers: []const Header, args: Args, response: *Response, connection: net.Server.Connection, alloc: std.mem.Allocator) !void {
     // std.debug.print("\nROUTE:: {s}\n", .{route});
-    var buf: [128]u8 = undefined;
 
     if (std.mem.startsWith(u8, route, "/echo/")) {
-        try connection.stream.writer().print("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {d}\r\n\r\n{s}", .{ route[6..].len, route[6..] });
+        // try connection.stream.writer().print("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {d}\r\n\r\n{s}", .{ route[6..].len, route[6..] });
+        response.code = ResponseCode.@"200";
+        response.content_type = "text/plain";
+        response.content = route[6..];
     } else if (std.mem.startsWith(u8, route, "/user-agent")) {
         for (headers) |header| {
-            // std.debug.print("\n{s}\n", .{header.name});
-            if (std.mem.eql(u8, std.ascii.lowerString(&buf, header.name), "user-agent")) {
-                try connection.stream.writer().print("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {d}\r\n\r\n{s}", .{ header.value.len, header.value });
+            if (std.ascii.eqlIgnoreCase(header.name, "user-agent")) {
+                // try connection.stream.writer().print("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {d}\r\n\r\n{s}", .{ header.value.len, header.value });
+                response.code = ResponseCode.@"200";
+                response.content_type = "text/plain";
+                response.content = header.value;
                 return;
             }
         }
-        try connection.stream.writeAll("HTTP/1.1 400 Bad Request\r\n\r\n");
+        // try connection.stream.writeAll("HTTP/1.1 400 Bad Request\r\n\r\n");
+        response.code = ResponseCode.@"400";
     } else if (std.mem.startsWith(u8, route, "/files/")) {
         if (args.directory) |directory| {
             var dir = std.fs.cwd().openDir(directory, .{}) catch |err| {
@@ -194,7 +234,8 @@ pub fn get(route: []const u8, headers: []const Header, args: Args, connection: n
             //TODO: Ensure the file is only in the allowed dir, no ../'ing
             const file = dir.readFileAlloc(alloc, route[7..], 2000) catch |err| {
                 if (err == error.FileNotFound) {
-                    try connection.stream.writeAll("HTTP/1.1 404 Not Found\r\n\r\n");
+                    // try connection.stream.writeAll("HTTP/1.1 404 Not Found\r\n\r\n");
+                    response.code = ResponseCode.@"404";
                 } else {
                     handleError(connection);
                 }
@@ -202,21 +243,25 @@ pub fn get(route: []const u8, headers: []const Header, args: Args, connection: n
             };
             std.debug.print("{s}", .{file});
 
-            const content_type = "application/octet-stream"; //if (std.mem.endsWith(u8, route, ".html")) "text/html" else "text/plain";
+            // const content_type = "application/octet-stream"; //if (std.mem.endsWith(u8, route, ".html")) "text/html" else "text/plain";
 
-            try connection.stream.writer().print("HTTP/1.1 200 OK\r\nContent-Type: {s}\r\nContent-Length: {d}\r\n\r\n{s}", .{ content_type, file.len, file });
+            // try connection.stream.writer().print("HTTP/1.1 200 OK\r\nContent-Type: {s}\r\nContent-Length: {d}\r\n\r\n{s}", .{ content_type, file.len, file });
+            response.code = ResponseCode.@"200";
+            response.content_type = "application/octet-stream";
+            response.content = file;
         } else { // A file is requested but the directory arg was not set.
             handleError(connection);
         }
     } else if (std.mem.eql(u8, route, "/")) {
-        try connection.stream.writeAll("HTTP/1.1 200 OK\r\n\r\n");
+        response.code = ResponseCode.@"200";
+        // try connection.stream.writeAll("HTTP/1.1 200 OK\r\n\r\n");
     } else {
-        try connection.stream.writeAll("HTTP/1.1 404 Not Found\r\n\r\n");
+        // try connection.stream.writeAll("HTTP/1.1 404 Not Found\r\n\r\n");
+        response.code = ResponseCode.@"404";
     }
 }
 
-pub fn post(route: []const u8, headers: []const Header, body: []const u8, args: Args, connection: net.Server.Connection, alloc: std.mem.Allocator) !void {
-    var buf: [128]u8 = undefined;
+pub fn post(route: []const u8, headers: []const Header, body: []const u8, args: Args, response: *Response, connection: net.Server.Connection, alloc: std.mem.Allocator) !void {
     const stderr = std.io.getStdErr();
     _ = alloc;
     std.debug.print("POSTING", .{});
@@ -229,9 +274,10 @@ pub fn post(route: []const u8, headers: []const Header, body: []const u8, args: 
 
             for (headers) |header| { // how do I stop it looping over the undefined areas
                 std.debug.print("\nhello, {s}", .{header.name});
-                if (std.mem.eql(u8, std.ascii.lowerString(&buf, header.name), "content-length")) {
+                if (std.ascii.eqlIgnoreCase(header.name, "content-length")) {
                     const content_length = std.fmt.parseUnsigned(u32, header.value, 10) catch |err| {
-                        try connection.stream.writeAll("HTTP/1.1 400 Bad Request\r\n\r\n");
+                        // try connection.stream.writeAll("HTTP/1.1 400 Bad Request\r\n\r\n");
+                        response.code = ResponseCode.@"400";
                         return err;
                     }; // error means that it was invalid int.
 
@@ -245,20 +291,18 @@ pub fn post(route: []const u8, headers: []const Header, body: []const u8, args: 
                     defer file.close();
                     try file.writeAll(body[0..content_length]);
 
-                    try connection.stream.writeAll("HTTP/1.1 201 Created\r\n\r\n");
+                    // try connection.stream.writeAll("HTTP/1.1 201 Created\r\n\r\n");
+                    response.code = ResponseCode.@"201";
                     return;
                 }
             }
 
-            try connection.stream.writeAll("HTTP/1.1 400 Bad Request\r\n\r\n");
+            // try connection.stream.writeAll("HTTP/1.1 400 Bad Request\r\n\r\n");
+            response.code = ResponseCode.@"400";
         } else { // A file is requested but the directory arg was not set.
             handleError(connection);
         }
     }
-}
-
-pub fn sendResponse(code: ResponseCode, content: ?[]u8, compression: u8, connection: net.Server.Connection) !void {
-
 }
 
 pub fn extractHeaders(headers: []Header, header_iterator: *std.mem.SplitIterator(u8, std.mem.DelimiterType.sequence)) ![]const Header {
